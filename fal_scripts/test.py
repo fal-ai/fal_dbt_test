8cd8e7b2f8871a6b4af0e7550d8fc3908ad61b2d
Meder <meder@kamalov.net>
2021-11-25 14:55:28 +0100

    Update test script

--- fal_scripts/test.py
+++ fal_scripts/test.py
@@ -1,1 +1,18 @@
-print(context.current_model.name)
+import pandas as pd
+import io
+
+model_name = context.current_model.name
+
+output = f"Model name: {model_name}"
+output = output + f"\nStatus: {context.current_model.status}"
+
+df = ref(model_name)
+buf = io.StringIO()
+df.info(buf=buf)
+info = buf.getvalue()
+
+output = output + f"\nModel dataframe informationz:\n{info}"
+
+f = open(f"fal_output/{model_name}", "w")
+f.write(output)
+f.close()
