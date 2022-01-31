37b5b47264a030364523bb9b4a02c18c33d2e1a5
Meder <meder@kamalov.net>
2022-01-31 11:22:54 +0100

    Include dbt test results

--- fal_scripts/test.py
+++ fal_scripts/test.py
@@ -2,17 +2,26 @@ import pandas as pd
 import io
 
 model_name = context.current_model.name
-
 output = f"Model name: {model_name}"
-output = output + f"\nStatus: {context.current_model.status}"
 
-df: pd.DataFrame = ref(model_name)
-buf = io.StringIO()
-df.info(buf=buf, memory_usage=False)
-info = buf.getvalue()
+if len(context.current_model.tests) > 0:
+    for test in context.current_model.tests:
+        output += f"\nRan {test.name} for {test.column}, result: {test.status}"
+    f = open(f"fal_output/{model_name}_test", "w")
+    f.write(output)
+    f.close()
 
-output = output + f"\nModel dataframe information:\n{info}"
+else:
+    output = f"Model name: {model_name}"
+    output = output + f"\nStatus: {context.current_model.status}"
 
-f = open(f"fal_output/{model_name}", "w")
-f.write(output)
-f.close()
+    df: pd.DataFrame = ref(model_name)
+    buf = io.StringIO()
+    df.info(buf=buf, memory_usage=False)
+    info = buf.getvalue()
+
+    output = output + f"\nModel dataframe information:\n{info}"
+
+    f = open(f"fal_output/{model_name}", "w")
+    f.write(output)
+    f.close()
